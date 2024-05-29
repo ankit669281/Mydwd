98c440fe73d013d08d6c4cc5691ee55469828698
ankit669281 <165649771+ankit669281@users.noreply.github.com>
2024-05-29 17:51:21 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,7 @@
 api_id = 26398147
 api_hash = "050ae2e744985b48fa37e8b2f38d7bea"
 bot_token = "7435452455:AAHDAJqa_S-MRUauVvUg6eOOO9jw_5K5tXo"
-auth_users = [7013332761]
+auth_users = [7013332761-6226272085]
 sudo_user = 7013332761
-log_channel = -1001791480211
-txt_channel = -1001791480211
+log_channel = -2148576139
+txt_channel = -4252886550
